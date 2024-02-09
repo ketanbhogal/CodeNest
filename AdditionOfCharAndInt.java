d8b0a5829e4dbc01b96a39547d86d90ad9c6bf22
ketanbhogal <155382696+ketanbhogal@users.noreply.github.com>
2024-02-09 12:08:40 +0530

    Add files via upload

---
+++ AdditionOfCharAndInt.java
@@ -0,0 +1,21 @@
+public class AdditionOfCharAndInt{   
+    public static void main(String []aoca){  
+        
+        
+ // Cheak the Adittion of number and character
+ // Ascii Value Of A is 65 And is Add With 10 Hance Answer is 75
+
+
+        char v='A';
+        int b=v+10;
+        System.out.println(b);
+    }
+        
+
+}
+
+/*output:-
+      compile Time = Success
+      Run Time = Sucess  
+      75
+*/
