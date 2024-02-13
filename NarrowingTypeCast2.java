ecc00fb933d5723d6ffee99f94b20654b5df0a5d
ketanbhogal <155382696+ketanbhogal@users.noreply.github.com>
2024-02-13 22:50:17 +0530

    Add files via upload

---
+++ NarrowingTypeCast2.java
@@ -0,0 +1,20 @@
+public class NarrowingTypeCast2 {
+    public static void main(String[]args){
+
+        //TypeCast Byte To int
+        //Explicite TypeCasting
+        double a=1000;
+        byte b=(byte)a;
+        System.out.println(b);
+    }
+    
+}
+
+
+
+            /*output:-
+                  compile Time = Success
+                  Run Time = Sucess  
+                  -24
+            
+             */
